b01b1b1392c3102f8e8c34883a3643827599a2e2
Admin <Admin@DESKTOP-4HG7AJ1>
2024-07-26 03:27:22 -0700

    this is my java working file

---
+++ java1/src/Day1_basics/Additiondemo.java
@@ -0,0 +1,44 @@
+package Day1_basics;
+import java.util.Scanner;
+public class Additiondemo {
+
+
+		// TODO Auto-generated method stub
+		public static void main(String[] args){
+
+			
+
+			int a,b,c;
+
+		    String n;
+
+		Scanner s=new Scanner(System.in);
+
+		a=s.nextInt();
+
+		b=s.nextInt();
+
+		s.nextLine();
+
+		System.out.println("enter the name");
+
+		n=s.next();
+
+		System.out.println("A="+a+"B="+b);
+
+
+
+		System.out.println("My name is :"+n);
+
+
+
+		}
+
+			}
+
+
+
+
+	
+
+
